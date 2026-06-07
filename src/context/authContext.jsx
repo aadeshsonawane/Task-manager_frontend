12e123db8b80f5df911906be51fc169fef44c45f
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-07 09:50:50 +0530

    create contect in authcontext to access in every components

---
+++ src/context/authContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('user')) || null
+  );
+  const [token, setToken] = useState(
+    localStorage.getItem('token') || null
+  );
